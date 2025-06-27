f37de041ae255a25c983a4fdc318755c8ce8a598
talabdar-hussain <htalabdar@gmail.com>
2025-06-27 17:25:04 +0500

    add concept of return

--- 01_functions/function.js
+++ 01_functions/function.js
@@ -1,22 +1,49 @@
 
 
 // function without perameter
-function myfunc(){
+// function myfunc(){
 
-    console.log("this is talabdar");
+//     console.log("this is talabdar");
     
-}
-console.log(myfunc());
+// }
+// console.log(myfunc());
 
 
 
 //function with perameters
 
 
-function myPerameterFun(number1,number2){
+// function myPerameterFun(number1,number2){
 
-    console.log(number1+number2);
+//     console.log(number1+number2);
+    
+// }
+
+// myPerameterFun(2,5)
+
+// function myFunction(num1,num2){
+
+//     // console.log(num1+num2);
+
+//     console.log(num1+num2);
+    
+    
+// }
+function myFunction(num1,num2){
+
+    // console.log(num1+num2);
+
+     const result=num1+num2
+    return result    
     
 }
 
-myPerameterFun(2,5)
+
+
+const result=myFunction(2,5)
+console.log(result);
+
+console.log(typeof result);
+
+
+
