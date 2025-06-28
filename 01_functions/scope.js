10024468853e066d032351b21de5080cb158340f
talabdar-hussain <htalabdar@gmail.com>
2025-06-28 22:05:26 +0500

    add scope concepts

---
+++ 01_functions/scope.js
@@ -0,0 +1,24 @@
+
+// let a =10
+// const b=20
+// // var c=30  
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+{}//this curli brakets shows scope
+
+var c =200
+
+if (true) {
+    
+let a =10
+const b=20
+var c=30  
+    
+}
+
+
+// console.log(a);  a is not defined
+// console.log(b);   is not defined
+console.log(c); // yeh c print ho rha ha mtlb var say jo variable initialize ho rha hai wo block scope ma b aghr hoga tou print hoga is leye let and const introduce howa
