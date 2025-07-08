0b3f6c51d986af009d63944280a2d2011049bc81
talabdar-hussain <htalabdar@gmail.com>
2025-07-08 18:56:31 +0500

    add control flow

---
+++ control_flow/01.js
@@ -0,0 +1,64 @@
+
+// if(condition){
+
+
+// }
+
+const value=true
+
+if (value) {
+    console.log("hello world");
+    
+    
+}
+
+if (value) {
+    console.log("condiionn is true");
+    
+    
+} else {
+    console.log("condition is false ");
+    
+    
+}
+
+
+// const isLogIn= true
+// const debitCard=true
+
+
+
+// // if(isLogIn && debitCard){
+// //     console.log("succesfully log");
+    
+// // }
+
+
+
+// if (isLogIn && debitCard) {
+    
+//     console.log("succesfully logIn");
+    
+    
+// } else {
+
+//     console.log("App logIn nae ho sakty");
+    
+    
+// }
+
+// const isLogIn= true
+// const debitCard=true
+// const anOther_value= false
+
+// if (isLogIn && debitCard && anOther_value) {
+    
+//     console.log("succesfully logIn");
+    
+    
+// } else {
+
+//     console.log("App logIn nae ho sakty");
+    
+    
+// }
