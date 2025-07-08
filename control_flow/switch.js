e422de9fd478f8a114cc5f4605b44f72d16b40cd
talabdar-hussain <htalabdar@gmail.com>
2025-07-08 19:13:53 +0500

    add concept of switch to codebase

---
+++ control_flow/switch.js
@@ -0,0 +1,68 @@
+
+
+
+
+const month="jan"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        
+        
+        break;
+    case "feb":
+        console.log("feb");
+        
+        
+        break;
+    case "march":
+        console.log("march");
+        
+        
+        break;
+    case "april":
+        console.log("april");
+        
+        
+        break;
+
+    default:
+        console.log("hi"); //yeh defualt code tab execute hoga jab koi value match nae hogi
+        
+
+
+        break;
+}
+
+
+// const month=4
+
+// switch (month) {
+//     case 1:
+//         console.log("january");
+        
+        
+//         break;
+//     case 2:
+//         console.log("feb");
+        
+        
+//         break;
+//     case 3:
+//         console.log("march");
+        
+        
+//         break;
+//     case 4:
+//         console.log("april");
+        
+        
+//         break;
+
+//     default:
+//         console.log("hi"); //yeh defualt code tab execute hoga jab koi value match nae hogi
+        
+
+
+//         break;
+// }
